44499a6ec64c5baff16e0d1f7ba23b8b63b9566c
Deepak Kanoujiya <deepakkanoujiya07@gmail.com>
2025-11-08 18:47:29 +0000

    remove completely this part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Hero from '@/components/landing/hero';
 import Features from '@/components/landing/features';
-import Stats from '@/components/landing/stats';
 import CallToAction from '@/components/landing/cta';
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Stats />
       <Features />
       <CallToAction />
     </>
