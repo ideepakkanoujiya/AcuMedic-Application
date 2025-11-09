dec67542d9e509c44de70dd9d14bf725993ad95a
Deepak Kanoujiya <deepakkanoujiya07@gmail.com>
2025-11-09 09:58:02 +0000

    implement the next usp feature

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,6 +1,8 @@
+'use server';
 import { config } from 'dotenv';
 config();
 
 import '@/ai/flows/ai-symptom-checker.ts';
 import '@/ai/flows/summarize-medical-report.ts';
 import '@/ai/flows/predictive-health-risk-flow.ts';
+
